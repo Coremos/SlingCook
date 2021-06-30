a804be2b4040538eb0b96672b5b5f991bb7d9a0e
Coremos <coremos@naver.com>
2021-06-30 16:20:51 +0900

    Update Linear.cs

--- Assets/Scripts/Linear.cs
+++ Assets/Scripts/Linear.cs
@@ -9,6 +9,11 @@ public class Linear : Singleton<Linear>
     public List<Vector3> waypointPosition = new List<Vector3>();
     public GameObject positionGameObject;
 
+
+    public GameObject orderPosition;
+    public List<bool> isOnOrderpoint = new List<bool>();
+    public List<Vector3> orderpointPosition = new List<Vector3>();
+
     private new void Awake()
     {
         var count = positionGameObject.transform.childCount;
@@ -17,13 +22,20 @@ public class Linear : Singleton<Linear>
             isOnWaypoint.Add(false);
             waypointPosition.Add(positionGameObject.transform.GetChild(i).transform.position);
         }
+
+        count = orderPosition.transform.childCount;
+        for (int i = 0; i < count; i++)
+        {
+            isOnOrderpoint.Add(false);
+            orderpointPosition.Add(orderPosition.transform.GetChild(i).transform.position);
+        }
     }
 }
 
 public class People : MonoBehaviour
 {
     int waypointIndex = 0;
-    enum State { None, Moving, Order }
+    enum State { None, Moving, MoveToOrder, Order, Exit }
     State state;
     float position;
 
@@ -40,11 +52,29 @@ public class People : MonoBehaviour
     {
         if (state == State.None)
         {
-            if (!Linear.instance.isOnWaypoint[waypointIndex + 1])
+            if (waypointIndex == Linear.instance.waypointPosition.Count - 1)
             {
-                Linear.instance.isOnWaypoint[waypointIndex++] = false;
-                Linear.instance.isOnWaypoint[waypointIndex] = true;
-                state = State.Moving;
+                
+                for (int i = 0; i < Linear.instance.isOnOrderpoint.Count; i++)
+                {
+                    // 해당 발판이 비어있을 때
+                    if (!Linear.instance.isOnOrderpoint[i])
+                    {
+                        Linear.instance.isOnOrderpoint[i] = true;
+                        waypointIndex = i;
+                        state = State.MoveToOrder;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                if (!Linear.instance.isOnWaypoint[waypointIndex + 1])
+                {
+                    Linear.instance.isOnWaypoint[waypointIndex++] = false;
+                    Linear.instance.isOnWaypoint[waypointIndex] = true;
+                    state = State.Moving;
+                }
             }
         }
         else if (state == State.Moving)
@@ -60,6 +90,18 @@ public class People : MonoBehaviour
                 position += Time.deltaTime;
             }
             
+        }
+        else if (state == State.MoveToOrder)
+        {
+            if (Vector3.Distance(transform.position, Linear.instance.orderpointPosition[waypointIndex]) < 1.0f)
+            {
+                state = State.Order;
+            }
+            else
+            {
+                Vector3.Lerp(transform.position, Linear.instance.orderpointPosition[waypointIndex], position);
+                position += Time.deltaTime;
+            }
         }
         
     }
